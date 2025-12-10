907e55a7a4a5dd29b4d5fa0bcd88b23d1cf6e52f
Lypyta <ly.samilla@duocuc.cl>
2025-12-10 05:19:16 -0300

    cambio de ruta proyecto

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\USUARIO\OneDrive - Fundacion Instituto Profesional Duoc UC\Escritorio\capstone\Capstone\venv"
+set "VIRTUAL_ENV=C:\Capstone\Capstone\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
